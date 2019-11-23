cf5381b38e43966377ef2a55691a7f28870985c1
User010 <User010@George93.lan>
2019-11-23 16:32:29 +0200

    changed

--- SnakeGame/src/controller/main.java
+++ SnakeGame/src/controller/main.java
@@ -21,7 +21,8 @@ public class main extends Application {
 	@Override
 	public void start(Stage ps) {
 
-		VBox root = new VBox(10); // the main view with 10px padding between elements
+		
+		VBox root = new VBox(20); // the main view with 10px padding between elements
 
 		PlayGround playGround = new PlayGround(Constants.GAME_WIDTH, Constants.GAME_HIGHT); // new play ground
 		playGround.addSnake(new Snake(Constants.SNAKE_LENGTH)); // adding snake
