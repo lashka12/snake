1bdf4384403dddabb444e84f879ca53378ea381e
L.A <L.A@DESKTOP-490V3G5>
2019-11-24 17:40:17 +0200

    adding comments to the main class

--- SnakeGame/src/controller/main.java
+++ SnakeGame/src/controller/main.java
@@ -7,13 +7,11 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import model.PlayGround;
 import model.Snake;
 import test.AcceptenceTests1;
 import utilities.Constants;
 import utilities.Direction;
-//import utilities.Direction;
 
 public class main extends Application {
 
@@ -23,13 +21,12 @@ public class main extends Application {
 	@Override
 	public void start(Stage ps) {
 
-
 		VBox root = new VBox(22); // the main view with 10px padding between elements
 		PlayGround playGround = new PlayGround(Constants.GAME_WIDTH, Constants.GAME_HIGHT); // new play ground
 		playGround.addSnake(new Snake(Constants.SNAKE_LENGTH)); // adding snake
 
 		timer = new AnimationTimer() { // the game frame updater timer
-			@Override 
+			@Override
 			public void handle(long now) {
 				if (now - then > 1000000000 / 50) {
 					playGround.update(); // update the hole game pane
@@ -42,11 +39,10 @@ public class main extends Application {
 
 		root.getChildren().add(playGround);
 		root.setPadding(new Insets(10));
-		Scene scene = new Scene(root); // will be changed to the main frame scene
-		//ps.initStyle(StageStyle.TRANSPARENT);
-		//scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
+		Scene scene = new Scene(root);
 
-		scene.setOnKeyPressed(e -> { // i can move this part the the mainFram controller to control the main scene
+		// this is the action listener that listen to the user keyboard input
+		scene.setOnKeyPressed(e -> { // will be moved later to the mainFram controller to control the main scene
 
 			if (e.getCode().equals(KeyCode.UP) && playGround.getSnake().getDirection() != Direction.DOWN)
 				playGround.getSnake().setDirection(Direction.UP);
@@ -60,15 +56,14 @@ public class main extends Application {
 		});
 
 		ps.setScene(scene);
-		// ps.setResizable(false);
 		ps.show();
 
 	}
 
 	public static void main(String[] args) { // will be moved to the main frame controller
-		
+
 		// activate the next code line to run tests
-		 //AcceptenceTests1.runTests();
+		// AcceptenceTests1.runTests();
 		launch(args); // start method will be called
 	}
 
