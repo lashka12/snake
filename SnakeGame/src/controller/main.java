952e1d5349d6214729c5a6521ba89b284a12ba12
L.A <L.A@DESKTOP-490V3G5>
2019-11-24 15:22:34 +0200

    vbox update

--- SnakeGame/src/controller/main.java
+++ SnakeGame/src/controller/main.java
@@ -22,11 +22,9 @@ public class main extends Application {
 
 	@Override
 	public void start(Stage ps) {
-		//comment from george
-		//hi george i saw your massage
 
 
-		VBox root = new VBox(14); // the main view with 10px padding between elements
+		VBox root = new VBox(20); // the main view with 10px padding between elements
 		PlayGround playGround = new PlayGround(Constants.GAME_WIDTH, Constants.GAME_HIGHT); // new play ground
 		playGround.addSnake(new Snake(Constants.SNAKE_LENGTH)); // adding snake
 
