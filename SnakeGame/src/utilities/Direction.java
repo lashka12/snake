020950371b56990ed48ae2ca81017fdac10b420d
samara <samara@DESKTOP-AMAQ3SJ>
2019-11-24 17:24:02 +0200

    commit 3 Enums.

---
+++ SnakeGame/src/utilities/Direction.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public enum Direction {
+	
+	UP, DOWN, LEFT, RIGHT;
+	
+}
---
+++ SnakeGame/src/utilities/FruiteType.java
@@ -0,0 +1,39 @@
+package utilities;
+
+import javafx.scene.image.Image;
+
+public enum FruiteType {
+
+	APPLE(5, 10, 1, Constants.APPLE_IMAGE), BANANA(10, 15, 1, Constants.BANANA_IMAGE),
+	PEAR(0, 0, 1, Constants.PEAR_IMAGE);
+
+	private final int secondsDelay;
+	private final int points;
+	private final int tailExtension;
+	private final Image image;
+
+	private FruiteType(int full, int abbr, int originalColony, Image image) {
+		this.secondsDelay = full;
+		this.points = abbr;
+		this.tailExtension = originalColony;
+		this.image = image;
+
+	}
+
+	public int getSecondsDelay() {
+		return secondsDelay;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public int getTailExtension() {
+		return tailExtension;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+}
